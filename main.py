d5f10cc739a9dc5dff089d9463b2b038be0fb93a
Song Jaehak <hpi@moonrabbit.co>
2015-07-10 01:03:40 +0900

    스도쿠 문제 로드까지

--- main.py
+++ main.py
@@ -3,12 +3,15 @@
 from kivy.app import App
 from kivy.clock import Clock
 from kivy.uix.screenmanager import ScreenManager, Screen
-from kivy.uix.widget import Widget
 from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.widget import Widget
 from kivy.lang import Builder
 from kivy.config import Config
-from kivy.properties import NumericProperty
-from datetime import timedelta
+from kivy.properties import NumericProperty, ObjectProperty
+
+from math import sqrt
+from board import Board, Problem
 
 Config.set('graphics', 'width', '320')
 Config.set('graphics', 'height', '480')
@@ -43,11 +46,71 @@ class SecondsLabel(Label):
         self.text = text
 
 
+class SudokuCell(Widget):
+    num = NumericProperty(0)
+
+
+class SudokuSquareGroup(GridLayout):
+    def __init__(self, **kwargs):
+        super(SudokuSquareGroup, self).__init__(**kwargs)
+        self.cells = []
+        for i in xrange(self.rows * self.cols):
+            cell = SudokuCell(num=i)
+            self.add_widget(cell)
+            self.cells.append(cell)
+
+
+class SudokuBoard(GridLayout):
+    board_model = ObjectProperty()
+
+    def on_board_model(self, instance, board):
+        self.clear_widgets()
+        size = board.size
+
+        sqrt_size = int(sqrt(size))
+        self.rows = sqrt_size
+        self.cols = sqrt_size
+
+        self.group_to_cells = []
+        self.cells = [[None for i in range(size)] for i in range(size)]
+
+        for i in xrange(size):
+            group = SudokuSquareGroup(
+                rows=sqrt_size, cols=sqrt_size)
+            self.add_widget(group)
+            self.group_to_cells.append(group.cells)
+
+        for group_idx, group in enumerate(self.group_to_cells):
+            for cell_idx, cell in enumerate(group):
+                group_row = group_idx / 3
+                group_col = group_idx % 3
+                cell_row = cell_idx / 3
+                cell_col = cell_idx % 3
+
+                i = group_row * 3 + cell_row
+                j = group_col * 3 + cell_col
+                print i, j
+                if self.cells[i][j] is not None:
+                    print self.cells[i][j]
+                self.cells[i][j] = cell
+
+        for i in range(size):
+            for j in range(size):
+                self.cells[i][j].num = int(board.problem[i][j])
+
+
 class InGameScreen(Screen):
     playtime = NumericProperty(0.0)
+    board = ObjectProperty()
 
     def on_enter(self):
         Clock.schedule_interval(self.update_playtime, 0.2)
+        problem = Problem.loads(
+            "800523910162489075350170420425008009690000057700600234037062041540317692016954003",
+            "874523916162489375359176428425738169693241857781695234937862541548317692216954783",
+            9)
+        board = Board(problem)
+        self.board.board_model = board
 
     def on_pre_leave(self):
         Clock.unschedule(self.update_playtime)
